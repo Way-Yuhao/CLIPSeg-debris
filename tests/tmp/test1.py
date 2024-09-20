1432daeba9a5172e6ab30bf3d28a7a9f0a10a6aa
Way-Yuhao <36289795+Way-Yuhao@users.noreply.github.com>
2024-09-20 12:50:17 -0500

    notebook runs ok

---
+++ tests/tmp/test1.py
@@ -0,0 +1,18 @@
+import os
+import autoroot
+import errno
+import torch
+import timeit
+import imageio
+import numpy as np
+import torch.nn as nn
+from torch.utils import data
+import matplotlib.pyplot as plt
+from tensorboardX import SummaryWriter
+from PIL import Image
+from src.utils.multi_annotators_utils.adamW import AdamW
+from src.utils.multi_annotators_utils.Loss import noisy_label_loss
+from src.utils.multi_annotators_utils.Utilis import segmentation_scores, CustomDataset_punet, calculate_cm
+from src.utils.multi_annotators_utils.Utilis import evaluate_noisy_label_4, evaluate_noisy_label_5, evaluate_noisy_label_6
+# our proposed model:
+from src.models.Models import UNet_CMs
