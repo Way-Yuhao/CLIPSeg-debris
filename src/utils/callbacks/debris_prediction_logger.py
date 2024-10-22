318bc1aa974b657d6d5df33682ead8a10b0d0f05
Way-Yuhao <36289795+Way-Yuhao@users.noreply.github.com>
2024-10-21 19:11:47 -0500

    rgb

--- src/utils/callbacks/debris_prediction_logger.py
+++ src/utils/callbacks/debris_prediction_logger.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 from PIL import Image
 import cv2
 from lightning.pytorch.callbacks import Callback
-
+from src.utils.clipseg_utils.gen_debris_vis_prompt import one_hot_encode_segmentation
 
 class DebrisPredictionLogger(Callback):
 
@@ -14,10 +14,12 @@ class DebrisPredictionLogger(Callback):
         self.save_dir = os.path.join(save_dir, 'predictions')
         self.onehot_save_dir = os.path.join(save_dir, 'onehot_rbg')
         os.makedirs(self.save_dir, exist_ok=True)
+        os.makedirs(self.onehot_save_dir, exist_ok=True)
 
     def on_predict_batch_end(self, trainer: "pl.Trainer", pl_module: "pl.LightningModule", outputs: Any,
                              batch: Any, batch_idx: int, dataloader_idx: int = 0) -> None:
         self.save_png(batch, outputs)
+        self.save_one_hot_rgb(batch, outputs)
 
     def save_png(self, batch: Any, outputs: Any):
         fname = os.path.join(self.save_dir, batch[1][2][0])
@@ -29,4 +31,12 @@ class DebrisPredictionLogger(Callback):
         # Image.fromarray(pred_class).save(fname)
 
     def save_one_hot_rgb(self, batch: Any, outputs: Any):
+        pred_class = outputs["pred_class"].squeeze().detach().cpu().numpy()
+        seg_low = pred_class == 1
+        seg_high = pred_class == 2
+        one_hot_segmentation = one_hot_encode_segmentation(seg_low, seg_high)
+        fname = os.path.join(self.onehot_save_dir, batch[1][2][0])
+        cv2.imwrite(fname, one_hot_segmentation)
+
+
 
